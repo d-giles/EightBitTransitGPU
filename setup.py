b4dc045652ad5377cb38499c6426a8eb012fe16d
Daniel Giles <daniel.k.giles@gmail.com>
2023-08-03 20:15:08 +0000

    Bump version: 2.0.3 → 2.0.4

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setup(name='EightBitTransit',
       include_package_data=True,
       test_suite='tests',
       tests_require=test_requirements,
-      version='2.0.3',
+      version='2.0.4',
       zip_safe=False,
       )
